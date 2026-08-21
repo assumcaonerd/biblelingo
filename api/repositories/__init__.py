4c961006d3df5fbba694305a868fee0fb912472f
assumcaonerd <assumcao@gmail.com>
2026-08-21 11:03:43 -0300

    feat(api): export SQLite vocabulary repository

--- api/repositories/__init__.py
+++ api/repositories/__init__.py
@@ -1,6 +1,11 @@
 """Repositórios de persistência da API."""
 
-from .progress_repo import ProgressRepository
 from .content_repo import ContentRepository
+from .progress_repo import ProgressRepository
+from .vocabulary_repo import VocabularyRepository
 
-__all__ = ["ProgressRepository", "ContentRepository"]
+__all__ = [
+    "ProgressRepository",
+    "ContentRepository",
+    "VocabularyRepository",
+]
